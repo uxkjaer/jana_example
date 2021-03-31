bafa714e9fb918a25262a065b5c5975208c4e22c
Jakob <jakob.m.kjaer@gmail.com>
2021-03-31 20:56:28 +1000

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/myorg/jana/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.myorg.jana.controller.MainView", {});
+});
